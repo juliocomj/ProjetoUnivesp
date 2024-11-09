09aabe6840cf985b8b35321719e5682d57566467
juliocomj <50389531+juliocomj@users.noreply.github.com>
2024-11-09 16:04:08 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,80 @@
+/*using ProjetoUNIVESP.Data;
+using ProjetoUNIVESP.Estudantes;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<AppDbContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+//CONFIGURANDO AS ROTAS
+app.AddRotasEstudantes();
+
+//app.AddMentoria();
+
+app.Run();
+*/
+
+using ProjetoUNIVESP.Data;
+using ProjetoUNIVESP.Estudantes;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<AppDbContext>();
+
+// Configuração do CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+// Ative a política de CORS
+app.UseCors("AllowAllOrigins");
+
+app.UseAuthorization();
+
+//CONFIGURANDO AS ROTAS
+app.AddRotasEstudantes();
+
+//app.AddMentoria();
+
+app.MapControllers();
+
+app.Run();
